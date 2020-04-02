e2b5f743f1c0545b6d8498052512fda11eb0a6e7
Jonathan Goldberg <gberg1303@aol.com>
2020-04-01 22:13:21 -0400

    Added robustness

--- PM_Demo_Bot.py
+++ PM_Demo_Bot.py
@@ -10,7 +10,7 @@ RealName = names.get_full_name()
 PlayerName = RealName[:10].replace(" ", "")
 email = RealName.replace(" ", "") + "@gmail.com"
 password = "PaSsWoRd12345!"
-location = "St. Louis"
+location = "Dresden 1945"
 
 class PM_Demo_Bot():
 
@@ -53,68 +53,97 @@ class PM_Demo_Bot():
             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
         except Exception: 
             try:
-                WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                 WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[51]/div[3]'))).click()# Seat 2
                 WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                 WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
             except Exception:
                 try:
-                    WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                    WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                     WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[62]/div[3]'))).click()# Seat 3
                     WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                     WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
                 except Exception:
                     try:
-                        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                        WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                         WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[73]/div[3]'))).click()# Seat 4
                         WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                         WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
                     except Exception:
                         try:
-                            WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                            WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[84]/div[3]'))).click()# Seat 5
                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
                         except Exception:
                             try:
-                                WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                                WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                                 WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[95]/div[3]'))).click()# Seat 6
                                 WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                                 WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
                             except Exception:
                                 try:
-                                    WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                                    WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                                     WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[106]/div[3]'))).click()# Seat 7
                                     WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                                     WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
                                 except Exception:
                                     try:
-                                        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                                        WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                                         WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[117]/div[3]'))).click()# Seat 8
                                         WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                                         WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
                                     except Exception:
                                         try:
-                                            WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                                            WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[128]/div[3]'))).click()# Seat 9
                                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
                                         except Exception:
-                                            WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
+                                            WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[64]/div[2]/div[5]/button'))).click() # Click Okay
                                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[139]/div[3]'))).click()# Seat 10
                                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[4]/div[2]/div[1]'))).click() # Click Max Buy-In
                                             WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[58]/div[2]/div[8]/button'))).click() # Click Okay
-    # Take a Seat that the Table
+    # Define Shove
     def Shove(self):
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[23]/div[1]/input'))).send_keys("10000") # Input Max Bid
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[26]/button'))).click() # Bid
+            
+    # Define Call
+    def Call(self):
+        WebDriverWait(self.driver,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[25]/button'))).click() # Call
+    
+    # Buy Back
+    def Buy_Back(self):
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[68]/div[2]/div[2]/button'))).click() # Click Okay
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[2]'))).click() # Click Menu
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[2]/ul/li[6]'))).click() # Click Add More Chips
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[56]/div[2]/div[2]/div[2]/div[1]'))).click() # Click Max Chips
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[56]/div[2]/div[5]/button'))).click() # Click Okay
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[2]/input'))).send_keys("Get ready to give me my money back!") # Input Max Bid
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[3]/button'))).click() # Bid
+
+    # Add an Introduction to the Bot
+    def Introduction(self):
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[2]/input'))).send_keys("How about a nice game of shovers?") # Input Max Bid
+        WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[3]/button'))).click() # Bid
+
+
+    def Play(self):
         while True:
             try:
-                WebDriverWait(self.driver,20000).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[23]/div[1]/input'))).send_keys("10000") # Input Max Bid
-                WebDriverWait(self.driver,20000).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[3]/div[55]/div[5]/div[26]/button'))).click() # Bid
+                self.Shove()
             except Exception:
-                self.driver.close()
-    
+                try:
+                    self.Call()
+                except Exception:
+                    try:
+                        self.Buy_Back()
+                    except Exception:
+                        self.Play()
         
 bot = PM_Demo_Bot()
 bot.Fuck_Zach_Friends()
 bot.Take_A_Seat()
-bot.Shove()
+bot.Introduction()
+bot.Play()
